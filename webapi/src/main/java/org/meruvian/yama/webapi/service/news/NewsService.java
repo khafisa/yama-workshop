80f845653a937fb04f1e5a80e739f753b7fe3a2b
khafi <romadhon.khafi@meruvian.org>
2015-12-26 17:43:02 +0700

    add swagger

--- webapi/src/main/java/org/meruvian/yama/webapi/service/news/NewsService.java
+++ webapi/src/main/java/org/meruvian/yama/webapi/service/news/NewsService.java
@@ -1,5 +1,8 @@
 package org.meruvian.yama.webapi.service.news;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -17,24 +20,34 @@ import javax.ws.rs.core.MediaType;
 import org.meruvian.yama.tot.news.News;
 
 @Path("/api/news")
+@Api("/news")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public interface NewsService {
 	@GET
 	@Path("/{id}")
+	@ApiOperation(httpMethod = "GET", value = "Get news by ID",
+    response = News.class)
 	News getNewsById(@PathParam("id") long id);
 	
 	@GET
+	@ApiOperation(httpMethod = "GET", value = "Find News by title", 
+    response = News.class, responseContainer = "Page")
 	List<News> findNewsByTitle(@QueryParam("title") @DefaultValue("") String title);
 	
 	@POST
+	@ApiOperation(httpMethod = "POST", value = "Post News", 
+    response = News.class)
 	News saveNews(News news);
 	
 	@PUT
 	@Path("/{id}")
+	@ApiOperation(httpMethod = "PUT", value = "PUT N by ID", 
+    response = News.class)
 	News updateNews(@PathParam("id") long id, News news);
 	
 	@DELETE
 	@Path("/{id}")
+	@ApiOperation(httpMethod = "DELETE", value = "Delete News by ID")
 	void deleteNews(@PathParam("id") long id);
 }
